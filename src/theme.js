45111797dd3b0c365e9802e54c07c3721d9a7714
Joao Dorino <js9978853@gmail.com>
2026-03-26 14:11:07 -0300

    fix footer + theme

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,23 @@
 import { createTheme, responsiveFontSizes } from "@mui/material";
 
 let theme = createTheme({
-    palette: {
-        primary: {
-            main: '#000000'
-        },
-        secondary: {
-            main: '#232323'
-        },
+  palette: {
+    primary: {
+      main: "#000000",
     },
-    typography: {
-        fontFamily: "Heebo"
-
-    }
+    secondary: {
+      main: "#232323",
+    },
+  },
+  typography: {
+    fontFamily: "Heebo, Arial, sans-serif",
+    h4: {
+      fontWeight: 600,
+    },
+    body1: {
+      fontSize: "16px",
+    },
+  },
 });
 
 theme = responsiveFontSizes(theme);
